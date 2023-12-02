8e402390b465bf26417393117afedfe715af3191
Reverse_lord <62026360+DjaInPentest@users.noreply.github.com>
2023-12-02 11:27:08 +0300

    Add files via upload

---
+++ read.py
@@ -0,0 +1,146 @@
+import random
+import tkinter as tk
+
+def generate_sudoku():
+    sudoku = [[0] * 9 for _ in range(9)]
+
+    def is_valid(num, row, col):
+        for i in range(9):
+            if sudoku[row][i] == num or sudoku[i][col] == num:
+                return False
+
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if sudoku[i + start_row][j + start_col] == num:
+                    return False
+
+        return True
+
+    def solve_sudoku():
+        for row in range(9):
+            for col in range(9):
+                if sudoku[row][col] == 0:
+                    nums = list(range(1, 10))
+                    random.shuffle(nums)
+                    for num in nums:
+                        if is_valid(num, row, col):
+                            sudoku[row][col] = num
+                            if solve_sudoku():
+                                return True
+                            sudoku[row][col] = 0
+                    return False
+        return True
+
+    solve_sudoku()
+    return sudoku
+
+class SudokuGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sudoku")
+        self.sudoku = None
+        self.solution = None
+        self.create_puzzle('easy')
+
+    def create_puzzle(self, difficulty):
+        if difficulty == 'easy':
+            num_cells_to_remove = 30
+        elif difficulty == 'medium':
+            num_cells_to_remove = 40
+        else:
+            num_cells_to_remove = 50
+
+        self.sudoku = generate_sudoku()
+        self.solution = [row[:] for row in self.sudoku]  # Store the solution for validation
+
+        # Remove cells to create the puzzle
+        cells = [(i, j) for i in range(9) for j in range(9)]
+        random.shuffle(cells)
+
+        for _ in range(num_cells_to_remove):
+            i, j = cells.pop()
+            self.sudoku[i][j] = 0
+
+        self.create_grid()
+
+    def create_grid(self):
+        self.entries = []
+        for i in range(9):
+            row = []
+            for j in range(9):
+                cell_value = self.sudoku[i][j]
+                if cell_value == 0:
+                    entry = tk.Entry(self.root, width=2, font=('Helvetica', 16))
+                    entry.grid(row=i, column=j)
+                    entry.bind('<KeyRelease>', lambda event, row=i, col=j: self.validate_entry(event, row, col))
+                    row.append(entry)
+                else:
+                    label = tk.Label(self.root, text=str(cell_value), font=('Helvetica', 16))
+                    label.grid(row=i, column=j)
+                    row.append(label)
+            self.entries.append(row)
+
+        validate_button = tk.Button(self.root, text="Validate", command=self.check_solution)
+        validate_button.grid(row=9, column=0, columnspan=9)
+
+    def validate_entry(self, event, row, col):
+        entry_value = self.entries[row][col].get()
+        if entry_value.isdigit() and 1 <= int(entry_value) <= 9:
+            self.sudoku[row][col] = int(entry_value)
+            self.entries[row][col].configure(bg='white')  # Reset background color
+        else:
+            self.sudoku[row][col] = 0
+            self.entries[row][col].configure(bg='red')  # Set background color to red
+
+        if self.sudoku[row][col] == self.solution[row][col]:
+            self.entries[row][col].configure(bg='green')  # Set background color to green for correct entries
+
+    def validate_solution(self):
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku[i][j] != self.solution[i][j]:
+                    return False
+        return True
+
+    def check_solution(self):
+        for i in range(9):
+            for j in range(9):
+                entry = self.entries[i][j]
+                if self.sudoku[i][j] == 0:
+                    if entry.get().isdigit() and int(entry.get()) == self.solution[i][j]:
+                        entry.configure(bg='green')  # Set background color to green for correct entries
+                    else:
+                        entry.configure(bg='red')  # Set background color to red for incorrect entries
+        if self.validate_solution():
+            for i in range(9):
+                for j in range(9):
+                    entry = self.entries[i][j]
+                    entry.configure(bg='green')  # Set background color to green for all correct entries
+            # You can add your logic for handling a correct solution here.
+            print("Congratulations! You solved the puzzle.")
+        else:
+            # You can add your logic for handling an incorrect solution here.
+            pass
+
+    def reset_puzzle(self):
+        for row in self.entries:
+            for cell in row:
+                cell.grid_forget()
+        self.create_puzzle('easy')
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = SudokuGame(root)
+
+    menu = tk.Menu(root)
+    root.config(menu=menu)
+    difficulty_menu = tk.Menu(menu)
+    menu.add_cascade(label="Difficulty", menu=difficulty_menu)
+    difficulty_menu.add_command(label="Easy", command=lambda: game.create_puzzle('easy'))
+    difficulty_menu.add_command(label="Medium", command=lambda: game.create_puzzle('medium'))
+    difficulty_menu.add_command(label="Hard", command=lambda: game.create_puzzle('hard'))
+    difficulty_menu.add_separator()
+    difficulty_menu.add_command(label="New Puzzle", command=game.reset_puzzle)
+
+    root.mainloop()
---
+++ test.py
@@ -0,0 +1,101 @@
+import random
+import tkinter as tk
+
+def generate_sudoku():
+    sudoku = [[0] * 9 for _ in range(9)]
+
+    def is_valid(num, row, col):
+        for i in range(9):
+            if sudoku[row][i] == num or sudoku[i][col] == num:
+                return False
+
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if sudoku[i + start_row][j + start_col] == num:
+                    return False
+
+        return True
+
+    def solve_sudoku():
+        for row in range(9):
+            for col in range(9):
+                if sudoku[row][col] == 0:
+                    nums = list(range(1, 10))
+                    random.shuffle(nums)
+                    for num in nums:
+                        if is_valid(num, row, col):
+                            sudoku[row][col] = num
+                            if solve_sudoku():
+                                return True
+                            sudoku[row][col] = 0
+                    return False
+        return True
+
+    solve_sudoku()
+    return sudoku
+
+class SudokuGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sudoku")
+        self.sudoku = None
+        self.create_puzzle('easy')
+
+    def create_puzzle(self, difficulty):
+        if difficulty == 'easy':
+            num_cells_to_remove = 30
+        elif difficulty == 'medium':
+            num_cells_to_remove = 40
+        else:
+            num_cells_to_remove = 50
+
+        self.sudoku = generate_sudoku()
+
+        # Remove cells to create the puzzle
+        cells = [(i, j) for i in range(9) for j in range(9)]
+        random.shuffle(cells)
+
+        for _ in range(num_cells_to_remove):
+            i, j = cells.pop()
+            self.sudoku[i][j] = 0
+
+        self.create_grid()
+
+    def create_grid(self):
+        self.entries = []
+        for i in range(9):
+            row = []
+            for j in range(9):
+                cell_value = self.sudoku[i][j]
+                if cell_value == 0:
+                    entry = tk.Entry(self.root, width=2, font=('Helvetica', 16))
+                    entry.grid(row=i, column=j)
+                    row.append(entry)
+                else:
+                    label = tk.Label(self.root, text=str(cell_value), font=('Helvetica', 16))
+                    label.grid(row=i, column=j)
+                    row.append(label)
+            self.entries.append(row)
+
+    def reset_puzzle(self):
+        for row in self.entries:
+            for cell in row:
+                cell.grid_forget()
+        self.create_puzzle('easy')
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = SudokuGame(root)
+
+    menu = tk.Menu(root)
+    root.config(menu=menu)
+    difficulty_menu = tk.Menu(menu)
+    menu.add_cascade(label="Difficulty", menu=difficulty_menu)
+    difficulty_menu.add_command(label="Easy", command=lambda: game.create_puzzle('easy'))
+    difficulty_menu.add_command(label="Medium", command=lambda: game.create_puzzle('medium'))
+    difficulty_menu.add_command(label="Hard", command=lambda: game.create_puzzle('hard'))
+    difficulty_menu.add_separator()
+    difficulty_menu.add_command(label="New Puzzle", command=game.reset_puzzle)
+
+    root.mainloop()
